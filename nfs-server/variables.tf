e548e98c234d5c1ef38a358ab07138eec9dffda7
Eric Vallee <eric_vallee@webificservices.com>
2023-03-01 05:42:45 -0500

    feat: Fixed problem with nfs server refusing high value ports. Restricted accepted domain for plaintext nfs connection to localhost

--- nfs-server/variables.tf
+++ nfs-server/variables.tf
@@ -17,7 +17,6 @@ variable "nfs_configs" {
   description = "List of nfs configurations containing a subset of possible nfs configurations. It is a list of objects, each entry containing the following fields: path, domain, rw (true or false), sync (true or false), subtree_check (true or false), no_root_squash (true or false)"
   type        = list(object({
     path = string
-    domain = string
     rw = bool
     sync = bool
     subtree_check = bool
