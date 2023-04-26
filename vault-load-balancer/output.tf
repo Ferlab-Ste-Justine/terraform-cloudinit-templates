c4327b030b90dd7be9ff80d3296745c1b440e7de
Sébastien Bonami <sbonami@ferlab.bio>
2023-04-26 14:45:59 -0400

    feat: INFRA-316 add container params to vault load balancer

--- vault-load-balancer/output.tf
+++ vault-load-balancer/output.tf
@@ -6,6 +6,10 @@ locals {
       haproxy = var.haproxy
     }
   )
+  container_params = {
+    fluentd = var.fluentd.tag != "" ? "--log-driver=fluentd --log-opt fluentd-address=127.0.0.1:${var.fluentd.port} --log-opt fluentd-retry-wait=1s --log-opt fluentd-max-retries=3600 --log-opt fluentd-sub-second-precision=true --log-opt tag=${var.fluentd.tag}" : ""
+    config = var.container_registry.url != "" ? "--config /opt/docker" : ""
+  }
 }
 
 output "configuration" {
@@ -17,6 +21,8 @@ output "configuration" {
       install_dependencies = var.install_dependencies
       tls                  = var.tls
       haproxy_config       = local.haproxy_config
+      container_params     = local.container_params
+      container_registry   = var.container_registry
     }
   )
 }
