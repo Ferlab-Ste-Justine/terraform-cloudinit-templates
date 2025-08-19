1207f035f5bd9511ea01999af4be031073070420
Sébastien Bonami <7561088+sebastienbonami@users.noreply.github.com>
2025-08-19 14:00:25 -0400

    feat: INFRA-1022 create s3-mounts template (#32)

---
+++ s3-mounts/variables.tf
@@ -0,0 +1,22 @@
+variable "install_dependencies" {
+  description = "Whether to install all dependencies in cloud-init"
+  type        = bool
+}
+
+variable "mounts" {
+  description = "List of s3 mounts with their configurations"
+  sensitive   = true
+  type        = list(object({
+    bucket_name   = string
+    access_key    = string
+    secret_key    = string
+    non_amazon_s3 = object({
+      url        = string
+      check_cert = bool
+    })
+    folder = object({
+      owner = string
+      umask = string
+    })
+  }))
+}
