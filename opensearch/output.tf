c050c24dcece8fa39f1c7e789e482b53437e641b
Issam Hannache <issam.hannache@icloud.com>
2025-11-17 13:34:57 -0500

    feat: INFRA-1332 Added opensearch audit logs

--- opensearch/output.tf
+++ opensearch/output.tf
@@ -1,30 +1,60 @@
 locals {
+  opensearch_audit_conf = var.opensearch_cluster.audit == null ? null : merge(
+    var.opensearch_cluster.audit,
+    {
+      enabled = try(var.opensearch_cluster.audit.enabled, false)
+    }
+  )
+
+  opensearch_initial_cluster_manager_nodes = (
+    try(length(var.opensearch_cluster.initial_cluster_manager_nodes), 0) > 0 ?
+    var.opensearch_cluster.initial_cluster_manager_nodes :
+    var.opensearch_cluster.seed_hosts
+  )
+
+  opensearch_cluster_with_defaults = merge(
+    var.opensearch_cluster,
+    local.opensearch_audit_conf == null ? {} : {
+      audit = local.opensearch_audit_conf
+    },
+    {
+      initial_cluster_manager_nodes = local.opensearch_initial_cluster_manager_nodes
+    }
+  )
+
+  opensearch_host_is_cluster_manager = var.opensearch_host.cluster_manager
+
+  opensearch_host_with_role = merge(
+    var.opensearch_host,
+    {
+      cluster_manager = local.opensearch_host_is_cluster_manager
+    }
+  )
+
   opensearch_bootstrap_conf = templatefile(
     "${path.module}/opensearch.yml.tpl",
     {
-      opensearch_cluster = var.opensearch_cluster
-      opensearch_host    = var.opensearch_host
+      opensearch_cluster = local.opensearch_cluster_with_defaults
+      opensearch_host    = local.opensearch_host_with_role
     }
   )
   opensearch_runtime_conf = templatefile(
     "${path.module}/opensearch.yml.tpl",
     {
-      opensearch_cluster = var.opensearch_cluster
-      opensearch_host    = {
-        bind_ip             = var.opensearch_host.bind_ip
-        extra_http_bind_ips = var.opensearch_host.extra_http_bind_ips
-        bootstrap_security  = var.opensearch_host.bootstrap_security
-        host_name           = var.opensearch_host.host_name
-        initial_cluster     = false
-        manager             = var.opensearch_host.manager
-      }
+      opensearch_cluster = local.opensearch_cluster_with_defaults
+      opensearch_host = merge(
+        local.opensearch_host_with_role,
+        {
+          initial_cluster = false
+        }
+      )
     }
   )
   opensearch_security_conf = {
     config = templatefile(
       "${path.module}/opensearch_security/config.yml.tpl",
       {
-        opensearch_cluster = var.opensearch_cluster
+        opensearch_cluster = local.opensearch_cluster_with_defaults
       }
     )
   }
@@ -33,11 +63,11 @@ locals {
 output "configuration" {
   description = "Cloudinit compatible opensearch configurations"
   value = templatefile(
-    "${path.module}/user_data.yaml.tpl", 
+    "${path.module}/user_data.yaml.tpl",
     {
       install_dependencies      = var.install_dependencies
-      opensearch_cluster        = var.opensearch_cluster
-      opensearch_host           = var.opensearch_host
+      opensearch_cluster        = local.opensearch_cluster_with_defaults
+      opensearch_host           = local.opensearch_host_with_role
       tls                       = var.tls
       opensearch_bootstrap_conf = local.opensearch_bootstrap_conf
       opensearch_runtime_conf   = local.opensearch_runtime_conf
--- opensearch/variables.tf
+++ opensearch/variables.tf
@@ -6,22 +6,56 @@ variable "opensearch_host" {
     bootstrap_security  = bool
     host_name           = string
     initial_cluster     = bool
-    manager             = bool
+    cluster_manager     = bool
   })
 }
 
 variable "opensearch_cluster" {
   description = "Opensearch cluster wide configurations"
   type = object({
-    auth_dn_fields      = object({
+    auth_dn_fields = object({
       admin_common_name = string
       node_common_name  = string
       organization      = string
     })
-    basic_auth_enabled  = bool
-    cluster_name        = string
-    seed_hosts          = list(string)
-    verify_domains      = bool
+    basic_auth_enabled            = bool
+    cluster_name                  = string
+    seed_hosts                    = list(string)
+    initial_cluster_manager_nodes = optional(list(string))
+    verify_domains                = bool
+
+    audit = optional(object({
+      enabled = optional(bool, false)
+      index   = string
+
+      external = optional(object({
+        http_endpoints = optional(list(string), [])
+        auth = optional(object({
+          ca_cert     = optional(string, "")
+          client_cert = optional(string, "")
+          client_key  = optional(string, "")
+          username    = optional(string, "")
+          password    = optional(string, "")
+          }), {
+          ca_cert     = ""
+          client_cert = ""
+          client_key  = ""
+          username    = ""
+          password    = ""
+        })
+        }), {
+        http_endpoints = []
+        auth = {
+          ca_cert     = ""
+          client_cert = ""
+          client_key  = ""
+          username    = ""
+          password    = ""
+        }
+      })
+
+      ignore_users = optional(list(string), [])
+    }), null)
   })
 }
 
@@ -38,6 +72,6 @@ variable "tls" {
 
 variable "install_dependencies" {
   description = "Whether to install all dependencies in cloud-init"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
